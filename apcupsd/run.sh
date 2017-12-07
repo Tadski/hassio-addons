8b558c52b62797ce922f58031d0a0ab6534e4266
Tadski <34170847+Tadski@users.noreply.github.com>
2017-12-07 09:45:09 +0000

    Update run.sh

--- apcupsd/run.sh
+++ apcupsd/run.sh
@@ -1,12 +1,11 @@
 #change the line below to allow person to edit name
 sed -i "s/#UPSNAME/UPSNAME APC Smart UPS/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/UPSCABLE ether/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/UPSTYPE net/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/DEVICE 192.168.100.116:3551/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/PCNET 192.168.100.116:3551/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/UPSCLASS standalone/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/UPSMODE disable/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#UPSCABLE/UPSCABLE ether/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#UPSTYPE/UPSTYPE net/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#NISIP/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#PCNET/PCNET 192.168.100.116:3551/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#UPSCLASS/UPSCLASS standalone/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/#UPSMODE/UPSMODE disable/g" /etc/apcupsd/apcupsd.conf
 echo "ISCONFIGURED=yes" >> /etc/default/apcupsd
 
 echo "$(apctest)"
