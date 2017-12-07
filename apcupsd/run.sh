7e22f01d056b86ad45c83ad957ddcf4dfe598dcb
Tadski <34170847+Tadski@users.noreply.github.com>
2017-12-07 09:25:46 +0000

    Update run.sh

--- apcupsd/run.sh
+++ apcupsd/run.sh
@@ -2,11 +2,11 @@
 sed -i "s/#UPSNAME/UPSNAME APC Smart UPS/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/UPSCABLE ether/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/UPSTYPE net/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/LOCKFILE /var/lock/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/DEVICE 192.168.100.116:3551/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/PCNET 192.168.100.116:3551/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/UPSCLASS standalone/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/UPSMODE disable/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/POLLTIME 10/g" /etc/apcupsd/apcupsd.conf
 echo "ISCONFIGURED=yes" >> /etc/default/apcupsd
 
 echo "$(apctest)"
